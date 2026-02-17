22473e573f85cdae68bdd721a7edb4ff180bbc8f
Matti Vaittinen <mazziesaccount@gmail.com>
2026-02-17 09:35:43 +0200

    rpi_shmem: Add shared mem buffer creation helpers
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

---
+++ rpi_shmem.c
@@ -0,0 +1,131 @@
+// Compile: gcc -o shm_server shm_server.c -lrt
+// Run: ./shm_server
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>      // O_* constants
+#include <sys/mman.h>   // shm_open, mmap
+#include <sys/stat.h>   // mode constants
+#include <unistd.h>
+#include <errno.h>
+
+#include <pthread.h>
+
+#include "rpi_shmem.h"
+
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+/* static struct g_shmem_info *g_i; */
+
+void shmem_close(struct shmem_info *info)
+{
+	if (!info)
+		return;
+
+	if (!info->buff)
+		return;
+
+	munmap(info->buff, info->size);
+
+	close(info->fd);
+}
+
+int shmem_open(const char *name, const size_t size, struct shmem_info *info)
+{
+	void *b;
+	int fd;
+
+	if (!info)
+		return -EINVAL;
+
+	fd = shm_open(name, O_RDONLY, 0);
+	if (fd == -1) {
+		int err = errno;
+
+		perror("shm_open");
+
+		return (err) ? err : -1;
+	}
+
+	b = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+	if (b == MAP_FAILED) {
+		int err = errno;
+
+		perror("mmap");
+		close(fd);
+
+		return (err) ? err : -1;
+	}
+
+	info->buff = b;
+	info->size = size;
+	info->fd = fd;
+	info->name = strdup(name);
+
+	return 0;
+}
+
+int shmem_create(const char *name, const size_t size, struct shmem_info *info)
+{
+	int fd;
+	void *b;
+
+	if (!info)
+		return -EINVAL;
+
+	if (!name || name[0] != '/')
+		return -EINVAL;
+
+	fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	if (fd == -1) {
+		int err = errno;
+
+		perror("shm_open");
+		return (err) ? err : -1;
+	}
+
+	if (ftruncate(fd, size) == -1) {
+		int err = errno;
+
+		perror("ftruncate");
+		close(fd);
+		shm_unlink(name);
+
+		return (err) ? err : -1;
+	}
+
+    
+	// Map into address space
+    
+	b = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
+	if (b == MAP_FAILED) {
+		int err = errno;
+
+		perror("mmap");
+		close(fd);
+		shm_unlink(name);
+
+		return (err) ? err : -1;
+	}
+
+	info->buff = b;
+	info->size = size;
+	info->fd = fd;
+	info->name = strdup(name);
+
+	return 0;
+}
+
+void shmem_destroy(struct shmem_info *i)
+{
+	if (!i || !i->buff)
+		return;
+
+	shmem_close(i);
+
+	if (shm_unlink(i->name) == -1)
+		perror("shm_unlink");
+}
+
---
+++ rpi_shmem.h
@@ -0,0 +1,20 @@
+#ifndef _MVA_RPI_SHMEM
+#define _MVA_RPI_SHMEM
+
+#include <stddef.h>
+
+struct shmem_info {
+	void *buff;
+	int fd;
+	const char *name;
+	size_t size;
+};
+
+int shmem_create(const char *name, const size_t size, struct shmem_info *info);
+int shmem_open(const char *name, const size_t size, struct shmem_info *info);
+void shmem_close(struct shmem_info *info);
+void shmem_destroy(struct shmem_info *info);
+
+
+
+#endif
