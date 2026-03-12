726c1f71f04f30b8f4e25963a02e5d646669e31f
Matti Vaittinen <mazziesaccount@gmail.com>
2026-03-12 15:31:05 +0200

    helpers: Add okteta editor structs to help binary analysis

---
+++ okteta/main.js
@@ -0,0 +1,31 @@
+function init(){
+	var adsample = struct({
+		data     : uint16(),
+		dummy    : uint16()
+	});
+	adsample.byteOrder = "big-endian";
+
+	var adsamples = array(adsample, 1024);
+	//adsamples.byteOrder = "big-endian";
+//	adsamples.byteOrder = "big-endian";
+
+	var record = struct({
+		usecs    : uint32(),
+//		samples  : array(uint32(), 1024),
+		datasamples  : adsamples,
+		gpiomask : array(uint32(), 1024)
+	});
+
+	var mvaring = struct({
+		version  : uint8(),
+		writing  : uint8(),
+		dropped  : uint16(),
+		size     : uint32(),
+		rindex   : uint32(),
+		windex   : uint32(),
+		adcdata : array(record, 100) //FIXME: size hardcoded, no length in format, read until EOF
+	});
+	mvaring.defaultLockOffset = 0;
+
+	return mvaring;
+}
